be3e29dc4994b5dafe1550a00f26f8714fe3a815
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-09-13 19:21:05 +0530

    data base connect and implement the route

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export function signup(req,res){
+    req.res.send("Signup Route");
+}
+export function login(req,res){
+    req.res.send("login Route");
+}
+export function logout(req,res){
+    req.res.send("logout Route");
+}
